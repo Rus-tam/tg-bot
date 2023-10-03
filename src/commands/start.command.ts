a9a8171010248d0b01e0c251816f74756a461c49
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-10-03 20:57:33 +0500

    handle command

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -1,5 +1,5 @@
 import { Command } from "./command.class";
-import { Telegraf } from "telegraf";
+import { Markup, Telegraf } from "telegraf";
 import { IBotContext } from "../context/context.interface";
 
 export class StartCommand extends Command {
@@ -7,5 +7,16 @@ export class StartCommand extends Command {
     super(bot);
   }
 
-  handle(): void {}
+  handle(): void {
+    this.bot.start((ctx) => {
+      console.log(ctx.session);
+      ctx.reply(
+        "Вам понравился курс?",
+        Markup.inlineKeyboard([
+          Markup.button.callback("Like", "course_like"),
+          Markup.button.callback("Dislike", "course_dislike"),
+        ]),
+      );
+    });
+  }
 }
